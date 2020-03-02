0c9cda04697f6640061e7cfa592f672b383f1be8
Willem Clarke <willem.2@hotmail.com>
2020-03-02 23:55:25 +1000

    first commit, setting up workspace, simple shaping of containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as _ from "lodash";
 
-function App() {
+import { Row, Col, Card } from "antd";
+import { Layout } from "antd";
+
+const { Header, Content } = Layout;
+
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ backgroundColor: "#cddddd", height: "100vh" }}>
+      <Header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: "40px",
+          fontFamily: "Trocchi, serif",
+          fontSize: "3rem"
+        }}
+      >
+        Markdown Previewer
+      </Header>
+      <Content>
+        <Row style={{ display: "flex", justifyContent: "center", paddingTop: "40px" }}>
+          <Col
+            span={12}
+            style={{
+              background: "red",
+              backgroundColor: "#e6f2f2",
+              padding: "20px 10px 20px 20px"
+            }}
+          >
+            <Card style={{ width: "500px", height: "700px", backgroundColor: "white", margin: "0px" }}>Cock</Card>
+          </Col>
+          <Col span={12} style={{ background: "red", backgroundColor: "#e6f2f2", padding: "20px 20px 20px 10px" }}>
+            <Card style={{ width: "500px", height: "700px", backgroundColor: "white", margin: "0px" }}>Cock</Card>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
   );
-}
+};
 
 export default App;
