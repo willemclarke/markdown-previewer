5ffeb1aa53a2f021fe99e7dac245a45cddc71ce9
Willem Clarke <willem.2@hotmail.com>
2020-03-05 14:57:01 +1000

    feat(useLocalStorage hook to preserve markdown state after refresh

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,17 @@ import React from "react";
 import useLocalStorage from "react-use-localstorage";
 import * as _ from "lodash";
 
-import { Row, Col } from "antd";
-import { Layout } from "antd";
 import { MarkdownInput } from "./components/MarkdownInput";
 import { HtmlPreview } from "./components/HtmlPreview";
+import { initialMarkdownState } from "./constants";
+
+import { Row, Col } from "antd";
+import { Layout } from "antd";
 
 const { Header, Content, Footer } = Layout;
 
 export const App: React.FC = () => {
-  const [markdown, setMarkdown] = React.useState(
-    `
-# Hello, This is Markdown Live Preview
-
-## what is Markdown?
-
-----
-
-see [Wikipedia](https://en.wikipedia.org/wiki/Markdown)
-
-> Markdown is a lightweight markup language, originally created by John Gruber and Aaron Swartz allowing people 
-  "to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)".
-
-## usage
-
-----
-
-1. Write markdown text in this textarea.
-2. See automatic preview here.
-
-## markdown quick reference
-
-----
-
-# headers
-
-*emphasis*
-
-**strong**
-
-* list
-
->block quote
-
-[links](https://wikipedia.org)
-
-## thanks
-
-----
-
-* [markdown-to-jsx](https://github.com/probablyup/markdown-to-jsx)
-  `
-  );
+  const [markdown, setMarkdown] = useLocalStorage("markdown", initialMarkdownState);
 
   return (
     <Layout style={{ backgroundColor: "#444444", height: "100vh" }}>
---
+++ src/constants.ts
@@ -0,0 +1,43 @@
+export const initialMarkdownState: string = `
+# Hello, This is Markdown Live Preview
+
+## what is Markdown?
+
+----
+
+see [Wikipedia](https://en.wikipedia.org/wiki/Markdown)
+
+> Markdown is a lightweight markup language, originally created by John Gruber and Aaron Swartz allowing people 
+  "to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)".
+
+## usage
+
+----
+
+1. Write markdown text in this textarea.
+2. See automatic preview here.
+
+## markdown quick reference
+
+
+----
+
+  
+# headers
+  
+*emphasis*
+  
+**strong**
+
+* list
+
+>block quote
+
+[links](https://wikipedia.org)
+
+## thanks
+
+----
+
+* [markdown-to-jsx](https://github.com/probablyup/markdown-to-jsx)
+`;
